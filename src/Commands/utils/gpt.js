a5e761e0a42561b8e840e94b8b16443a60a88dd8
Deep Eagle <130247297+Deep-Eagle@users.noreply.github.com>
2023-04-13 02:35:42 +0530

    ᴅᴇᴇᴘ ᴇᴀɢʟᴇ

---
+++ src/Commands/utils/gpt.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'gpt',
+    aliases: ['g'],
+    category: 'utils',
+    exp: 5,
+    description: 'Let you chat with GPT chat bot',
+    async execute(client, arg, M) {
+        if (!arg) return M.reply('Sorry you did not give any term!')
+        if (!client.openaiAPI) return M.reply('You did not provided any api key for OpenAI useage!')
+        const res = await client.AI.gpt(arg).catch((err) => {
+            return M.reply(err.toString())
+            client.log(err, 'red')
+        })
+        M.reply(res.response)
+    }
+}
