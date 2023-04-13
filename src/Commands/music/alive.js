6419c3ae6c96b7f4a0313b77ce0bb4130b79c91b
Deep Eagle <130247297+Deep-Eagle@users.noreply.github.com>
2023-04-13 19:45:02 +0530

    ᴅᴇᴇᴘ ᴇᴀɢʟᴇ

--- src/Commands/music/alive.js
+++ src/Commands/music/alive.js
@@ -6,9 +6,22 @@ module.exports = {
     description: 'Testing stuff',
     async execute(client, arg, M) {
         M.reply(
-            `හැම දෙයක්ම වැඩ කරනවා ${
-                (await client.contact.getContact(M.sender, client)).පරිශීලක නාමය
-            } | *Exp:* ${await client.exp.get(M.sender)} *Level:* ${(await client.DB.get(`${M.sender}_LEVEL`)) || 0}`
+            `හැම දෙයක්ම වැඩ කරනවා`
+ // send a buttons message!
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1},
+  {buttonId: 'id3', buttonText: {displayText: 'Button 3'}, type: 1}
+]
+
+const buttonMessage = {
+    text: "Hi it's button message",
+    footer: 'Hello World',
+    buttons: buttons,
+    headerType: 1
+}
+
+const sendMsg = await sock.sendMessage(id, buttonMessage)
         )
     }
 }
